29fe88a09d5bd30b955d89d5ce3bda8c87e8dc7e
Tobbe Lundberg <tobbe@tlundberg.com>
2018-08-05 12:45:31 +0200

    Add a title to user pages

--- NF_text_corrections.user.js
+++ NF_text_corrections.user.js
@@ -25,4 +25,24 @@
         const warmCoolLi = document.querySelector('.format_text ol li:nth-child(4)');
         warmCoolLi.innerHTML = warmCoolLi.innerHTML.replace('50', '40');
     }
+
+    if (window.location.href === 'https://www.nerdfitness.com/level-up/my-epic-quest/') {
+        document.title = 'My Epic Quest';
+    }
+
+    if (window.location.href === 'https://www.nerdfitness.com/level-up/my-quests/') {
+        document.title = 'Quests';
+    }
+
+    if (window.location.href === 'https://www.nerdfitness.com/level-up/my-character/') {
+        document.title = 'My Character';
+    }
+
+    if (window.location.href === 'https://www.nerdfitness.com/my-account/') {
+        document.title = 'My Account';
+    }
+
+    if (window.location.href === 'https://www.nerdfitness.com/level-up/courses/') {
+        document.title = 'Courses';
+    }
 })();
