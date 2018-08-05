acc080bd88cdcf61e6afa810c2f8ae188545d531
Tobbe Lundberg <tobbe@tlundberg.com>
2018-08-05 19:49:23 +0200

    Version 0.2

--- NF_text_corrections.user.js
+++ NF_text_corrections.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nerd Fitness text corrections
 // @namespace    https://github.com/tobbe
-// @version      0.1
+// @version      0.2
 // @description  Corrects small text misstakes on NF
 // @license      MIT
 // @author       Tobbe
