e6a90a96654eda7b709d0151fd2ff92123faff68
Tobbe Lundberg <tobbe@tlundberg.com>
2018-08-16 14:33:44 +0200

    Re-order source code

--- NF_text_corrections.user.js
+++ NF_text_corrections.user.js
@@ -12,20 +12,6 @@
 (function() {
     'use strict';
 
-    if (window.location.href === 'https://academy.nerdfitness.com/week-three-missions/') {
-        const batcaveLi = document.querySelector('.format_text ol li:nth-child(1)');
-        batcaveLi.innerHTML = batcaveLi.innerHTML.replace('25', '40');
-
-        const teamLi = document.querySelector('.format_text ol li:nth-child(2)');
-        teamLi.innerHTML = teamLi.innerHTML.replace('25', '40');
-
-        const drinkLi = document.querySelector('.format_text ol li:nth-child(3)');
-        drinkLi.innerHTML = drinkLi.innerHTML.replace('50', '60');
-
-        const warmCoolLi = document.querySelector('.format_text ol li:nth-child(4)');
-        warmCoolLi.innerHTML = warmCoolLi.innerHTML.replace('50', '40');
-    }
-
     if (window.location.href === 'https://academy.nerdfitness.com/week-1-missions/') {
         const measurementsLi = document.querySelector('.format_text ol li:nth-child(2)');
         measurementsLi.innerHTML = measurementsLi.innerHTML.replace('25', '40');
@@ -70,6 +56,20 @@
         dietLi.innerHTML = dietLi.innerHTML
             .replace('the&nbsp;NF', 'the NF')
             .replace('passed!).<a', 'passed!). <a');
+    }
+
+    if (window.location.href === 'https://academy.nerdfitness.com/week-three-missions/') {
+        const batcaveLi = document.querySelector('.format_text ol li:nth-child(1)');
+        batcaveLi.innerHTML = batcaveLi.innerHTML.replace('25', '40');
+
+        const teamLi = document.querySelector('.format_text ol li:nth-child(2)');
+        teamLi.innerHTML = teamLi.innerHTML.replace('25', '40');
+
+        const drinkLi = document.querySelector('.format_text ol li:nth-child(3)');
+        drinkLi.innerHTML = drinkLi.innerHTML.replace('50', '60');
+
+        const warmCoolLi = document.querySelector('.format_text ol li:nth-child(4)');
+        warmCoolLi.innerHTML = warmCoolLi.innerHTML.replace('50', '40');
     }
 
     if (window.location.href === 'https://www.nerdfitness.com/level-up/my-epic-quest/') {
